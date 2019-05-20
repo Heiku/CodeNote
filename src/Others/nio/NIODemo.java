253eaf7c83fcecfecd64fc6e72ca3c2517e5175e
Heiku <1154034741@qq.com>
2019-05-20 20:55:12 +0800

    nio note

---
+++ src/Others/nio/NIODemo.java
@@ -0,0 +1,15 @@
+package Others.nio;
+
+/**
+ * @Author: Heiku
+ * @Date: 2019/5/20
+ *
+ * NIO 面向缓冲区，对 data 的操作基本通过 buffer 进行
+ * IO  面向流， 对 data 的操作通过 stream
+ *
+ * NIO 主要由 Channel + Buffer + Selector
+ *
+ */
+
+public class NIODemo {
+}
