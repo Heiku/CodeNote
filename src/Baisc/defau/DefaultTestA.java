d45e0b551c913b1a476b9df0a3db474f92d5a1d1
linjiahong <heiiku@qq.com>
2019-09-18 18:40:49 +0800

    future && interface default && cpu cache

---
+++ src/Baisc/defau/DefaultTestA.java
@@ -0,0 +1,43 @@
+package Baisc.defau;
+
+/**
+ * @Author: Heiku
+ * @Date: 2019/9/17
+ */
+
+interface InterfaceA{
+    default void foo(){
+        System.out.println("InterfaceA foo");
+    }
+}
+
+interface InterfaceB extends InterfaceA{
+
+}
+
+interface InterfaceC extends InterfaceA{
+    @Override
+    default void foo() {
+        System.out.println("InterfaceC foo");
+    }
+}
+
+interface InterfaceD extends InterfaceA{
+    @Override
+    void foo();
+}
+
+
+public class DefaultTestA {
+    public static void main(String[] args) {
+
+        new InterfaceB(){}.foo();       // InterfaceA foo
+        new InterfaceC(){}.foo();       //  InterfaceC foo
+        new InterfaceD(){
+            @Override
+            public void foo() {
+                System.out.println("InterfaceD foo");
+            }
+        }.foo();                        // InterfaceD foo
+    }
+}
---
+++ src/Baisc/defau/DefaultTestB.java
@@ -0,0 +1,42 @@
+package Baisc.defau;
+
+/**
+ * @Author: Heiku
+ * @Date: 2019/9/17
+ */
+interface InterfaceBA{
+    default void foo(){
+        System.out.println("InterfaceA foo");
+    }
+}
+
+interface InterfaceBB{
+    default void bar(){
+        System.out.println("InterfaceB bar");
+    }
+}
+
+interface InterfaceBC{
+    default void foo(){
+        System.out.println("InterfaceC foo");
+    }
+
+    default void bar(){
+        System.out.println("InterfaceC bat");
+    }
+}
+
+class ClassA implements InterfaceA, InterfaceB {
+}
+
+public class DefaultTestB implements InterfaceBB, InterfaceBC {
+    @Override
+    public void bar() {
+        InterfaceBB.super.bar(); // 调用 InterfaceB 的 bar 方法
+        InterfaceBC.super.bar(); // 调用 InterfaceC 的 bar 方法
+    }
+
+    public static void main(String[] args) {
+        // 如果想调用确切的接口方法，使用 Interface.super.method()
+    }
+}
