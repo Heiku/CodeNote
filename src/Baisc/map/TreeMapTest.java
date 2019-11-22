80dd3bda4bb58008155ff79a1cec8dc434d541ae
linjiahong <heiiku@qq.com>
2019-11-22 21:22:59 +0800

    update mysql knowledge

---
+++ src/Baisc/map/TreeMapTest.java
@@ -0,0 +1,23 @@
+package Baisc.map;
+
+import java.util.TreeMap;
+
+/**
+ * @Author: Heiku
+ * @Date: 2019/11/22
+ */
+public class TreeMapTest {
+    public static void main(String[] args) {
+
+        TreeMap<Integer, String> map = new TreeMap<>();
+        map.put(1, "a");
+        map.put(3, "b");
+        map.put(5, "c");
+        map.put(7, "d");
+        map.put(9, "e");
+
+        System.out.println(map.ceilingEntry(0).getValue());
+        System.out.println(map.ceilingEntry(7).getValue());
+        System.out.println(map.ceilingEntry(8).getValue());
+    }
+}
