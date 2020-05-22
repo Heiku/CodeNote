9590dc09d2a8e9eb0117523e6fe9e722070d3765
Heiku <1154034741@qq.com>
2020-05-23 00:04:45 +0800

    threadPoolExecutor

---
+++ src/Basic/cpu/CPUTest.java
@@ -0,0 +1,11 @@
+package Basic.cpu;
+
+/**
+ * @author Heiku
+ * @date 2020/5/18
+ **/
+public class CPUTest {
+    public static void main(String[] args) {
+        System.out.println(Runtime.getRuntime().availableProcessors());
+    }
+}
